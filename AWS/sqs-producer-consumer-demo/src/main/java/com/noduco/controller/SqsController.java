f142cd62ba2cbc266c886c0ab9e670bc93b7832e
Rohit9252 <rohitbatra0165@gmail.com>
2024-09-30 13:31:33 +0530

    aws multiple consumer example

---
+++ AWS/sqs-producer-consumer-demo/src/main/java/com/noduco/controller/SqsController.java
@@ -0,0 +1,18 @@
+package com.noduco.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.noduco.service.SqsConsumerService1;
+
+@RestController
+@RequestMapping("/sqs")
+public class SqsController {
+	
+
+    
+
+}
